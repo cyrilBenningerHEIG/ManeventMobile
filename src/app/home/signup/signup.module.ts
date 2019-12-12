31c333fa0232a044480b96fb76a14f4267a8ce56
Alvaro Baptista <alvaro.baptista@heig-vd.ch>
2019-12-12 10:54:31 +0100

    sign up page

--- src/app/home/signup/signup.module.ts
+++ src/app/home/signup/signup.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { SignupPageRoutingModule } from './signup-routing.module';
 
 import { SignupPage } from './signup.page';
+import { NotInValidatorDirective } from '../../not-in.directive';
 
 @NgModule({
   imports: [
@@ -15,13 +17,15 @@ import { SignupPage } from './signup.page';
     IonicModule,
     SignupPageRoutingModule
   ],
-  declarations: [SignupPage]
+  declarations: [SignupPage, NotInValidatorDirective]
 })
 export class SignupPageModule {
   greeting: string;
   displayedGreeting: string;
-    displayGreeting() {
-    this.displayedGreeting = this.greeting;
+    displayGreeting(form: NgForm) {
+      if (form.valid) {
+        this.displayedGreeting = this.greeting;
     console.log('Greeting displayed');
+      }
   }
 }
