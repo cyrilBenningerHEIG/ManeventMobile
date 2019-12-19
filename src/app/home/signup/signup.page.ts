b956f2d698c53ae8243c7bbee2eb24a4ada5d8eb
Cyril Benninger <cyril.benninger@heig-vd.ch>
2019-12-19 08:57:34 +0100

    add user complete

--- src/app/home/signup/signup.page.ts
+++ src/app/home/signup/signup.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { User } from '../../models/user';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class SignupPage {
+  newUser: User;
+  userError: boolean;
+  constructor(private http: HttpClient,private router: Router) {
+    this.newUser = new User();
   }
+  
+  onSubmit(form: NgForm) {
+    // Do not do anything if the form is invalid.
+    if (form.invalid) {
+      return;
+    }
 
+    const AddUserURL = '/api/users';
+    this.http.post(AddUserURL, this.newUser).subscribe({
+      next: () => {
+      this.router.navigateByUrl('/home');
+    },
+    error: err => {
+      this.userError = true;
+      console.warn(`Authentication failed: ${err.message}`);
+    }
+  });
+      
+    
+  }
 }
+
+
