49d190a91580691c1248da23d6fd65773fa9090f
Cyril Benninger <cyril.benninger@heig-vd.ch>
2020-01-09 15:31:59 +0100

    store event with geodata

--- src/app/home/event/create/create.page.ts
+++ src/app/home/event/create/create.page.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router';
 
 import { Event } from '../../../models/event';
 
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.page.html',
@@ -14,6 +15,8 @@ import { Event } from '../../../models/event';
 })
 export class CreatePage {
   newEvent: Event;
+
+
   constructor(private auth: AuthService, private http: HttpClient, private router: Router) {
     this.newEvent = new Event();
   }
@@ -23,20 +26,37 @@ export class CreatePage {
       console.log("ta race");
     }
 
-    const AddUserURL = '/api/events';
-    
-    this.http.post(AddUserURL, this.newEvent).subscribe({
-      next: () => {
-        console.log(this.newEvent);
-      },
-      error: err => {
-        //this.userError = true;
-        console.warn(`Authentication failed: ${err.message}`);
-      }
-    });
+    const NominatimURL = "https://nominatim.openstreetmap.org/search?q=";
+    this.http.get(NominatimURL + this.newEvent["address"] + '&format=geojson&limit=1').subscribe(result => {
+      let geodata = result['features']['0']['geometry'];
+
+      this.newEvent['location'] = {
+        'type': geodata['type'],
+        'coordinates': geodata['coordinates']
+      };
+      const AddUserURL = '/api/events';
+      this.http.post(AddUserURL, this.newEvent).subscribe({
+        next: () => {
+          console.log(this.newEvent);
+        },
+        error: err => {
+          //this.userError = true;
+          console.warn(`Authentication failed: ${err.message}`);
+
+        }
+      });
+      
+    })
+
+
 
 
   }
 
-
 }
+
+
+
+
+
+
--- src/app/models/event.ts
+++ src/app/models/event.ts
@@ -1,17 +1,8 @@
 export class Event {
     name: string;
     date: string;
-    location: {
-        type: {
-          type: String, // Don't do { location: { type: String } }
-          enum: ['Point'], // 'location.type' must be 'Point'
-          required: true
-        },
-        coordinates: {
-          type: [Number],
-          required: true
-        }
-    };
+    address:string;
+    
     image: string;
     time: string;
     description: string;
