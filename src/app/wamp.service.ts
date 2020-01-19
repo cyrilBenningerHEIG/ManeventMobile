f2483adaec82659317f14842e829851b924d8aaf
Ksenia <ksenia.mironova.95@gmail.com>
2020-01-19 13:44:15 +0100

    wamp update

--- src/app/wamp.service.ts
+++ src/app/wamp.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Connection, Session } from 'autobahn-browser';
 import { ReplaySubject } from 'rxjs';
-import { `Observable, Observer,` ReplaySubject } from 'rxjs';
+// import { `Observable, Observer,` ReplaySubject } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
 const wampUrl = 'wss://wamp.archidep.media/ws ';
@@ -22,24 +22,24 @@ export class WampService {
     connection.open();
   }
 
-  public `call(procUri: string, arr?: any[], obj?: object, options?: object)`: Observable<any> {
-    // Retrieve the WAMP session
-    return this.session$.pipe(
-      switchMap(session => {
-        // Convert the RPC promise to an Observable equivalent
-        return new Observable((subscriber: Observer<any>) => {
-         session
-            // Make the call
-          .call(procUri, arr, obj, options)
-            // If the call succeeds, emit the returned value
-           .then(subscriber.next.bind(subscriber))
-            // If the call fails, emit the error
-           .catch(subscriber.error.bind(subscriber))
-            // Whatever happens, unsubscribe from the Observable at the end
-           .finally(subscriber.complete.bind(subscriber));
-        });
-      })
-    );
-  }
+  // public `call(procUri: string, arr?: any[], obj?: object, options?: object)`: Observable<any> {
+  //   // Retrieve the WAMP session
+  //   return this.session$.pipe(
+  //     switchMap(session => {
+  //       // Convert the RPC promise to an Observable equivalent
+  //       return new Observable((subscriber: Observer<any>) => {
+  //        session
+  //           // Make the call
+  //         .call(procUri, arr, obj, options)
+  //           // If the call succeeds, emit the returned value
+  //          .then(subscriber.next.bind(subscriber))
+  //           // If the call fails, emit the error
+  //          .catch(subscriber.error.bind(subscriber))
+  //           // Whatever happens, unsubscribe from the Observable at the end
+  //          .finally(subscriber.complete.bind(subscriber));
+  //       });
+  //     })
+  //   );
+  // }
 
 }
