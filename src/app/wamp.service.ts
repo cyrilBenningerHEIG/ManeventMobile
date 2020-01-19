5f65580571b0d99b42cbf78202461d0db7fcf83a
Ksenia <ksenia.mironova.95@gmail.com>
2020-01-19 18:52:44 +0100

    WAMP update

--- src/app/wamp.service.ts
+++ src/app/wamp.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Connection, Session } from 'autobahn-browser';
-import { ReplaySubject } from 'rxjs';
-// import { `Observable, Observer,` ReplaySubject } from 'rxjs';
+import { Observable, Observer, ReplaySubject} from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 
-const wampUrl = 'wss://wamp.archidep.media/ws ';
-const wampRealm = 'manevent';
-
+const secret = 'diagram-despite-joint-oxygen';
 
 @Injectable({ providedIn: 'root' })
 export class WampService {
@@ -15,31 +12,34 @@ export class WampService {
 
   constructor() {
     // Create the connection to the WAMP Brocker
-    const connection = new Connection({ url: wampUrl, realm: wampRealm, authid: 'manevent', authmethods: ['ticket'], ticket: 'diagram-despite-joint-oxygen'});
+    const connection = new Connection({ url: 'wss://wamp.archidep.media/ws', realm: 'manevent', authid: 'manevent', authmethods: ['ticket'], onchallenge: function () {
+      return secret }});
     // When the connection is open, emit the session through the ReplaySubject
     connection.onopen = session => this.session$.next(session);
     // Initiate the connection
     connection.open();
+    console.log("HEllo from WAMP Service")
   }
 
-  // public `call(procUri: string, arr?: any[], obj?: object, options?: object)`: Observable<any> {
-  //   // Retrieve the WAMP session
-  //   return this.session$.pipe(
-  //     switchMap(session => {
-  //       // Convert the RPC promise to an Observable equivalent
-  //       return new Observable((subscriber: Observer<any>) => {
-  //        session
-  //           // Make the call
-  //         .call(procUri, arr, obj, options)
-  //           // If the call succeeds, emit the returned value
-  //          .then(subscriber.next.bind(subscriber))
-  //           // If the call fails, emit the error
-  //          .catch(subscriber.error.bind(subscriber))
-  //           // Whatever happens, unsubscribe from the Observable at the end
-  //          .finally(subscriber.complete.bind(subscriber));
-  //       });
-  //     })
-  //   );
-  // }
+  public call(procUri: string, arr?: any[], obj?: object, options?: object): Observable<any> {
+    // Retrieve the WAMP session
+    return this.session$.pipe(
+      switchMap(session => {
+        // Convert the RPC promise to an Observable equivalent
+        return new Observable((subscriber: Observer<any>) => {
+         session
+            // Make the call
+          .call(procUri, arr, obj, options)
+            // If the call succeeds, emit the returned value
+           .then(subscriber.next.bind(subscriber))
+            // If the call fails, emit the error
+           .catch(subscriber.error.bind(subscriber))
+            // Whatever happens, unsubscribe from the Observable at the end
+           .finally(subscriber.complete.bind(subscriber));
+        });
+      })
+    );
+    console.log('hello from call')
+  }
 
 }
